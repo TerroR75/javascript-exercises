2d081c1bdf827825460f319e20e2b96155162236
TerroR75 <kamilsulimierski@gmail.com>
2022-05-10 15:15:58 +0200

    exercises 01-08 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+    let endResult = '';
+    if (number > 0) {
+        for (let i = 0; i < number; i++) {
+            endResult += string;
+        }
+        return endResult;
+    }
+    else if (number < 0) {
+        return 'ERROR';
+    }
+    else {
+        return '';
+    }
 
 };
 
