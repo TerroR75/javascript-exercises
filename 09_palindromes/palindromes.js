4b463a2c28c53ec45463bc9c3e14c00fdcd5e328
TerroR75 <kamilsulimierski@gmail.com>
2022-05-11 13:38:04 +0200

    10th done - not knowing simpler solution though

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,45 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let transformedString = '';
+    let reversedString = '';
 
+    // Transforming string to lower case and without whitespaces
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] != ' ' && string[i] != '!' && string[i] != '?' && string[i] != '.' && string[i] != ',') {
+            transformedString += string[i].toLowerCase();
+        }
+    }
+
+    // Reversing string
+    for (let i = transformedString.length - 1; i >= 0; i--) {
+        reversedString += transformedString[i];
+    }
+
+    // console.log('Transformed: ', transformedString);
+    // console.log('Reversed: ', reversedString);
+
+    // Checking palindrome
+    for (let i = 0; i < transformedString.length; i++) {
+        // console.log('i:  ', transformedString[i]);
+        // console.log('rev i: ', reversedString[i]);
+        if (transformedString[i] != reversedString[i]) {
+            return false;
+        }
+    }
+    return true;
+
+
+
+
+
+
+    // SIMPLE SOLUTION
+    processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+        processedString
+            .split("")
+            .reverse()
+            .join("") == processedString
+    );
 };
 
 // Do not edit below this line
